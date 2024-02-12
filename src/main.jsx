23e443ca797f25ca5588800b29ccc8c6279c4ff1
Reahana <reahana233@gmail.com>
2024-02-12 20:31:48 +0600

    main

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,30 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Home from './components/Home/Home.jsx';
+import Countries from './components/Countries/Countries.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children:[
+      {
+        path:"/",
+        element: <Countries></Countries>,
+        loader:()=>fetch ('https://restcountries.com/v3.1/all')
+      },
+      {
+        path:"country/:countryId",
+        element: <div>okkk</div>,
+    loader:({params})=>fetch (`/${params.countryId}`)
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
